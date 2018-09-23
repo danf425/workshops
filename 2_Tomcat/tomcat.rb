2e9e84527b81be6ff0caee31915c703480163682
danf425 <danf425@gmail.com>
2018-09-22 22:14:02 -0400

    + change in access and permissions

--- 2_Tomcat/tomcat.rb
+++ 2_Tomcat/tomcat.rb
@@ -22,3 +22,27 @@ remote_file '/tmp/apache-tomcat-8.5.9.tar.gz' do
     source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.9/bin/apache-tomcat-8.5.9.tar.gz'
 end
 
+#sudo mkdir /opt/tomcat
+directory '/opt/tomcat' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+#extract tar file
+execute '/tmp/apache-tomcat-8.5.9.tar.gz' do
+  command 'tar -xzvf /tmp/apache-tomcat-8.5.9.tar.gz '\
+    '--strip-components=1'
+  cwd '/opt/tomcat'
+end
+
+#chmod
+bash 'tomcat_config' do
+  code <<-EOH
+chgrp -R tomcat /opt/tomcat
+chmod -R g+r /opt/tomcat/conf
+chmod g+x /opt/tomcat/conf
+chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs/
+EOH
+end
