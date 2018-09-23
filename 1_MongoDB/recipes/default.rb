4e2fd4bcd76c4f451ff3ed00409be18c4793a010
Daniel Flores <vagrant@localhost.localdomain>
2018-09-23 21:42:10 +0000

    cookbook version

---
+++ 1_MongoDB/recipes/default.rb
@@ -0,0 +1,67 @@
+##########################################################################
+# Cookbook Name:: mongodb
+# Recipe:: install
+#
+# Not sure how to get started?
+#
+# You could:
+# 1.  copy the relevant commands from http://docs.mongodb.org/manual/tutorial/install-mongodb-on-red-hat-centos-or-fedora-linux/
+# 2.  comment out everything
+# 3.  add the Chef resources and other Chef code necessary
+#
+# This file is an example of steps 1 and 2 above.
+##########################################################################
+
+#run cmd:
+##sudo chef-client --local-mode default.rb
+
+case node['platform']
+###INSTALLATION/SETUP FOR RHEL
+when 'redhat', 'centos'
+
+# Create a /etc/yum.repos.d/mongodb.repo file to hold the following configuration information for the MongoDB repository:
+file 'etc/yum.repos.d/mongodb-org.repo' do
+  content '[mongodb]
+name=MongoDB Repository
+baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/
+gpgcheck=0
+enabled=1'
+end
+
+# Install the MongoDB packages and associated tools.
+yum_package 'mongodb-org' do
+  action :upgrade
+end
+
+# Start MongoDB + ensure that MongoDB will start following a system reboot
+service 'mongod' do
+  action [:enable,:start]
+end
+
+##INSTALLATION/SETUP FOR UBUNTU
+when 'ubuntu'
+###UBUNTU-14.04-TRUSTY
+bash 'install_ubuntu' do
+  code <<-EOH
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+apt-get update
+apt-get install -y mongodb-org
+EOH
+end
+
+# Start MongoDB + ensure that MongoDB will start following a system reboot
+service 'mongod' do
+  action [:enable,:start]
+end
+
+end
+
+
+#Resources used: 
+#1)https://learn.chef.io/modules/learn-the-basics/rhel/virtualbox/set-up-a-machine-to-manage#/
+#   CentOS7 + Vagrant + VirtualBox + ChefSDK + Vim
+#2)https://docs.chef.io/resource_reference.html
+#3)https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+#4) http://docs.mongodb.org/manual/tutorial/install-mongodb-on-red-hat-centos-or-fedora-linux/
+
---
+++ 2_Tomcat/recipes/default.rb
@@ -0,0 +1,120 @@
+#####Module 2: Tomcat###########
+#run cmd:
+##sudo chef-client --local-mode default.rb
+
+
+case node['platform']
+###INSTALL JDK1.7 for RHEL || CENTOS
+when 'redhat', 'centos'
+   yum_package 'java-1.7.0-openjdk-devel' do
+		action :upgrade
+   end
+##INSTALL JDK1.8 FOR UBUNTU
+when 'ubuntu'
+   apt_update
+   apt_package 'openjdk-8-jre' do
+		action :upgrade
+	end
+end
+
+#Create a user for tomcat
+group 'tomcat' do
+   action :create
+end
+
+user 'tomcat' do
+   action :create
+   gid 'tomcat'
+   home '/opt/tomcat'
+   shell '/bin/nologin'
+end
+
+#Download the Tomcat Binary
+remote_file '/tmp/apache-tomcat-8.5.9.tar.gz' do
+    source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.9/bin/apache-tomcat-8.5.9.tar.gz'
+end
+
+#sudo mkdir /opt/tomcat
+directory '/opt/tomcat' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+#extract tar file
+execute '/tmp/apache-tomcat-8.5.9.tar.gz' do
+  command 'tar -xzvf /tmp/apache-tomcat-8.5.9.tar.gz '\
+    '--strip-components=1'
+  cwd '/opt/tomcat'
+  not_if { File.exists?("/opt/tomcat/RELEASE-NOTES") }
+end
+
+#chmod
+bash 'tomcat_config' do
+  code <<-EOH
+chgrp -R tomcat /opt/tomcat
+chmod -R g+r /opt/tomcat/conf
+chmod g+x /opt/tomcat/conf
+chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs/
+EOH
+end
+
+case node['platform']
+#Install the Systemd Unit File + reload + start + enable FOR RHEL+CENTOS
+when 'redhat', 'centos'
+	systemd_unit 'tomcat.service' do
+	  content({Unit: {
+				Description:'Apache Tomcat Web Application Container',
+				After:'syslog.target network.target',
+			  },
+			  Service: {
+				Type:'forking',
+				Environment:'JAVA_HOME=/usr/lib/jvm/jre',
+				Environment:'CATALINA_PID=/opt/tomcat/temp/tomcat.pid',
+				Environment:'CATALINA_HOME=/opt/tomcat',
+				Environment:'CATALINA_BASE=/opt/tomcat',
+				Environment:'CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC',
+				Environment:'JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom',
+				ExecStart:'/opt/tomcat/bin/startup.sh',
+				ExecStop:'/bin/kill -15 $MAINPID',
+				User:'tomcat',
+				Group:'tomcat',
+				UMask:'0007',
+				RestartSec:10,
+				Restart:'always',
+			  },
+			  Install: {
+				WantedBy:'multi-user.target',
+			  }})
+	  action [:create, :reload, :start, :enable]
+	end
+#Install the Systemd Unit File + reload + start + enable FOR UBUNTU
+when 'ubuntu'
+	systemd_unit 'tomcat.service' do
+	  content({Unit: {
+				Description:'Apache Tomcat Web Application Container',
+				After:'syslog.target network.target',
+			  },
+			  Service: {
+				Type:'forking',
+				Environment:'JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre',
+				Environment:'CATALINA_PID=/opt/tomcat/temp/tomcat.pid',
+				Environment:'CATALINA_HOME=/opt/tomcat',
+				Environment:'CATALINA_BASE=/opt/tomcat',
+				Environment:'CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC',
+				Environment:'JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom',
+				ExecStart:'/opt/tomcat/bin/startup.sh',
+				ExecStop:'/bin/kill -15 $MAINPID',
+				User:'tomcat',
+				Group:'tomcat',
+				UMask:'0007',
+				RestartSec:10,
+				Restart:'always',
+			  },
+			  Install: {
+				WantedBy:'multi-user.target',
+			  }})
+	  action [:create, :reload, :start, :enable]
+	end
+end
