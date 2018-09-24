f8c6cf71051c7a50523c4e774746fb4cd8cf326f
Daniel Flores <vagrant@localhost.localdomain>
2018-09-24 00:01:24 +0000

    chefspec testing for mongodb

--- 1_MongoDB/spec/default_spec.rb
+++ 1_MongoDB/spec/default_spec.rb
@@ -3,9 +3,24 @@ require 'chefspec'
 
 
 describe '1_MongoDB::default' do
-  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+  let(:chef_run) do
+    ChefSpec::SoloRunner.new(platform: 'centos', version: '7.5.1804')
+                          .converge(described_recipe)
+  end
 
-  it 'installs mongodb-org' do
-    expect(chef_run).to upgrade_yum_package('mongodb-org')
+  it 'creates a file for mongodb' do
+      expect(chef_run).to create_file('etc/yum.repos.d/mongodb-org.repo')
+  end
+
+  it 'installs a yum_package mongodb-org' do
+#     expect(chef_run).to install_yum_package('mongodb-org')
+      expect(chef_run).to upgrade_yum_package('mongodb-org')
+  end
+
+  it 'starts and enables mongod service' do
+      expect(chef_run).to enable_service('mongod')
+      expect(chef_run).to start_service('mongod')
+#####      expect(chef_run).to stop_service('mongod') 
   end
 end
+
--- 1_MongoDB/spec/spec_helper.rb
+++ 1_MongoDB/spec/spec_helper.rb
@@ -2,13 +2,3 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-describe '1_MongoDB::default' do
-  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
-
-  it 'creates repo file' do
-    expect(chef_run).to create_file('etc/yum.repos.d/mongodb-org.repo')
-  end
-end
-
-
-
