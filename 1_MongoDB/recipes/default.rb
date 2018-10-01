929105e0a27264ceedbb912db9b7caeebf61d34a
danf425 <danf425@gmail.com>
2018-09-30 20:26:07 -0400

    Update "not_if" to Bash -ubuntu
    Adding conditional so that the entire 'Public_key+source_list' bash script doesn't run in its entirety after the first time

--- 1_MongoDB/recipes/default.rb
+++ 1_MongoDB/recipes/default.rb
@@ -42,6 +42,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 apt-get update
 EOH
+not_if { File.exists?("/etc/apt/sources.list.d/mongodb-org-3.0.list") }
 end
 
 apt_package "mongodb-org" do
